5dc6d0e7eac3f0e5ffcea5617331781a33f797cb
Amy <33025632+theamanraj@users.noreply.github.com>
2020-11-10 20:45:09 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,25 @@
+# Create an arbitrary local resource
+data "template_file" "test" {
+  template = "Hello, I am a template. My sample_var value = $${sample_var}"
+
+  vars = {
+    sample_var = var.sample_var
+  }
+}
+
+# This is just to make sure that ibm provider is instantiated
+/* data "ibm_schematics_workspace" "test" {
+  workspace_id = "my-workspace-id"
+}
+*/
+
+resource "null_resource" "sleep" {
+  triggers = {
+    uuid = uuid()
+  }
+
+  provisioner "local-exec" {
+    command = "sleep ${var.sleepy_time}"
+  }
+}
+
---
+++ versions.tf
@@ -0,0 +1,12 @@
+
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    null = {
+      source = "hashicorp/null"
+    }
+    template = {
+      source = "hashicorp/template"
+    }
+  }
+}

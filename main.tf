dcf8d03da9e50cb3635387259c12c3099a5ced1e
Amy <33025632+theamanraj@users.noreply.github.com>
2020-11-10 19:28:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,22 @@
+resource "ibm_compute_ssh_key" "test_key_1" {
+  label      = "test_key_1"
+  public_key = "var.ssh_public_key"
+}
+
 # Create an arbitrary local resource
 data "template_file" "test" {
   template = "Hello, I am a template. My sample_var value = $${sample_var}"
 
   vars = {
-    sample_var = var.x
+    sample_var = var.sample_var
   }
 }
+
+# This is just to make sure that ibm provider is instantiated
+/* data "ibm_schematics_workspace" "test" {
+  workspace_id = "my-workspace-id"
+}
+*/
 
 resource "null_resource" "sleep" {
   triggers = {
@@ -16,4 +27,3 @@ resource "null_resource" "sleep" {
     command = "sleep ${var.sleepy_time}"
   }
 }
-

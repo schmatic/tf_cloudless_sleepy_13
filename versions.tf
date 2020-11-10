00e7e74d03195b69546752e590ea442ff5e3b42b
Amy <33025632+theamanraj@users.noreply.github.com>
2020-11-10 19:26:07 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,4 +1,12 @@
-
 terraform {
-  required_version = ">= 0.13"
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+      version = "1.13.1"
+    }
+  }
+}
+
+provider "ibm" {
+  
 }

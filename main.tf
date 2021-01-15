c2e90b595fb4d198bf3e6254f6314dde6da58de6
Amy <33025632+theamanraj@users.noreply.github.com>
2021-01-15 21:40:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,8 @@
-# Create an arbitrary local resource
 resource "ibm_iam_access_group" "accgrp" {
   name        = "test"
 }
 
+provider "ibm" {
+  ibmcloud_api_key =  var.ibmcloud_api_key
+  generation       = 2
+}

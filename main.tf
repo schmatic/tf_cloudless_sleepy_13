782575c0e21f7af1c26d8b49a6295a3b6555bfd8
Amy <33025632+theamanraj@users.noreply.github.com>
2021-01-18 13:12:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "ibm_iam_access_group" "accgrp" {
   name        = "test"
 }
 
-provider "ibm" {
-  ibmcloud_api_key =  var.ibmcloud_api_key
-  generation       = 2
-}
+# provider "ibm" {
+#   ibmcloud_api_key =  var.ibmcloud_api_key
+#   generation       = 2
+# }
